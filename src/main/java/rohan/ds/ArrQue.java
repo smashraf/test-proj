b30f9f66a539d7560b8443d427fccb0f675a80fd
matrix1992 <piyush.scm@gmail.com>
2015-05-28 20:18:39 +0530

    added and consolidated some questions

--- src/main/java/rohan/ds/ArrQue.java
+++ src/main/java/rohan/ds/ArrQue.java
@@ -114,6 +114,49 @@ public class ArrQue {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    
+    public void spiralPrint(int m, int n, int a[][]) {
+        int i, k = 0, l = 0;
+        
+       
+     
+        while (k < m && l < n)
+        {
+            /* Print the first row from the remaining rows */
+            for (i = l; i < n; ++i)
+            {
+                System.out.print(a[k][i]+" ");
+            }
+            k++;
+     
+            /* Print the last column from the remaining columns */
+            for (i = k; i < m; ++i)
+            {
+                System.out.print(a[i][n-1]+" " );
+            }
+            n--;
+     
+            /* Print the last row from the remaining rows */
+            if ( k < m)
+            {
+                for (i = n-1; i >= l; --i)
+                {
+                    System.out.print(a[m-1][i]+" ");
+                }
+                m--;
+            }
+     
+            /* Print the first column from the remaining columns */
+            if (l < n)
+            {
+                for (i = m-1; i >= k; --i)
+                {
+                    System.out.print(a[i][l]+" ");
+                }
+                l++;    
+            }        
+        }
+    }
 
     public boolean hasTriplet(int[] arr, int n) {
         if (arr.length < 3)
